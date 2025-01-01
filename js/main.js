b934e1708d8682f6ebd24c35c01356b26341fe4c
yroner <yroner@mail.ru>
2025-01-01 23:59:43 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper",{
+    effect: "fade",
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
