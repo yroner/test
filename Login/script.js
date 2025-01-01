4a16012ac28583aedb0c2f84d57a99d8d58ba75e
yroner <yroner@mail.ru>
2025-01-01 23:58:56 +0300

    Add files via upload

---
+++ Login/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("home_btn");
+
+button.addEventListener('click', () => {
+    // Переход на другую страницу
+    window.location.href = '../index.html'; // Замените на URL вашей страницы
+});
